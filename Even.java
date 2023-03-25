2a682bca4e628b20e0a2b3e727036e29c97910ce
ReshmaKakde <128820179+ReshmaKakde@users.noreply.github.com>
2023-03-25 17:21:19 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,13 @@
+// Check the number is even or odd
+class Even{
+public static void main(String [] args)
+{
+int a=10;
+if(a%2==0)
+{
+System.out.println("The given number is even number");
+}
+else
+System.out.println("the given number is odd number");
+}
+}
